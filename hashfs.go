9a182481fef107046185f8fb2ce6023f5c950b92
c9845 <>
2023-07-02 15:49:10 -0400

    Start working on rewriting this again.
    Basically going to rewrite everything:
     - Adding more and better code comments explaining functionality.
     - Remove exported funcs that really aren't needed to be exported.
     - General code cleanup.
     - Better example code (how to use for HTTP router, {{static}} template func, etc.

--- hashfs.go
+++ hashfs.go
@@ -23,14 +23,17 @@ var _ fs.FS = (*FS)(nil)
 // data since the filename will change if the data changes.
 type FS struct {
 	fsys fs.FS
+	mu   sync.RWMutex
 
-	mu sync.RWMutex
-	m  map[string]string    // lookup (path to hash path)
-	r  map[string][2]string // reverse lookup (hash path to path)
+	//Lookup maps.
+	pathToHashPath map[string]string    // lookup (path to hash path)
+	r              map[string][2]string // reverse lookup (hash path to path)
 
-	hl hashLocation
+	//Options.
+	hashLocation hashLocation
 }
 
+// hashLocation defines the position of the hash in the filename.
 type hashLocation int
 
 const (
@@ -38,11 +41,17 @@ const (
 	hashLocationFirstPeriod                     //script.min.js -> script-a1b2c3...d4e5f6.min.js; original designed hash location
 	hashLocationEnd                             //script.min.js -> script.min.a1b2c3...d4e5f6.js
 
+	//default is "at first period in filename" since that was the legacy location of
+	//the hash.
 	hashLocationDefault = hashLocationFirstPeriod
 )
 
+// optionFunc is a function used to modify the way the FS works. For example, setting
+// the location of the hash in the filename.
 type optionFunc func(*FS)
 
+// NewFS returns an FS for working with hashed static files. Options modify the way
+// the FS works when hashing or serving files.
 func NewFS(fsys fs.FS, options ...optionFunc) *FS {
 	f := &FS{
 		fsys: fsys,
@@ -51,12 +60,111 @@ func NewFS(fsys fs.FS, options ...optionFunc) *FS {
 		hl:   hashLocationDefault,
 	}
 
+	//Apply any options.
 	for _, option := range options {
 		option(f)
 	}
 
 	return f
 }
+
+// HashedPath returns the path to a file with the hash of the file added to the
+// file's filename. The hash of the file is calculated here and cached for the
+// future.
+//
+// If a file doesn't exist at the filepath, an error is returned.
+func (hf *FS) HashedPath(filepath string) (filepathWithHash string, err error) {
+	//Check if the file's hash has already been calculated and cached. This
+	//alleviates us from having to calculate the hash each time a file is requested.
+	hf.mu.RLock()
+	p, exists := hf.pathToHashPath[filepath]
+	if exists {
+		filepathWithHash = p
+		return
+	}
+
+	//Look up the file at the filepath to make sure it exists and, if it does, so
+	//we can calculate the hash.
+	b, err := fs.ReadFile(hf.fsys, filepath)
+	if err != nil {
+		return
+	}
+
+	//Calculate the hash.
+	//TODO: support other hash functions? S3 uses MD5?
+	hash := sha256.Sum256(b)
+
+	//Encode the hash.
+	//TODO: support other encodings?
+	encoded := hex.EncodeToString(hash[:])
+
+	//Format the filename with the hash.
+
+}
+
+func (hf *FS) formatFilepathWithHash(filepath, hash string) (filepathWithHash string) {
+	//Quick validation.
+	if filepath == "" {
+		return
+	}
+	if hash == "" {
+		return
+	}
+
+	//Split the filepath into path and filename components. We need the filename
+	//separately since that is where the hash is added to.
+	dir, filename := path.Split(filepath)
+
+	//Add the hash to the filename.
+	switch hf.hashLocation {
+	case hashLocationFirstPeriod:
+		//Handle if the filename doesn't have a period in it. This shouldn't really
+		//ever occur since a filename should have an extension. In this case, just
+		//append the hash to the filename, separating it with a dash to make it
+		//stand out a bit.
+		i := strings.Index(filename, ".")
+		if i == -1 {
+			filename = dir + "-" + hash
+			break
+		}
+
+		//Add the hash just before the first period, separating it with a dash to
+		//make it stand out a bit.
+		filename = filename[:i] + "-" + hash + filename[i:]
+
+	case hashLocationStart:
+		//Add the hash to the beginning of the filename, separating it with a dash
+		//to make it stand out a bit.
+		filename = hash + "-" + filename
+
+	case hashLocationEnd:
+		//Add the hash to the end of the filename, duplicating the file's extension
+		//after the hash to prevent breaking MIME type determination in browsers.
+		//The filename and hash is separated with a dash to make it stand out a bit
+		//but the extension is copied/appended with a period since that obviously
+		//makes sense.
+		//
+		//Note, path.Ext() returns a value starting with a period (i.e.: .css).
+		filename = filename + "-" + hash + path.Ext(filename)
+
+	default:
+		//This should never occur since fsys.hashLocation is set by default and
+		//can only be set to one of our defined values. This is just here since all
+		//switches should have a default.
+		filename = ""
+	}
+
+	filepathWithHash = path.Join(dir, filename)
+	return
+}
+
+//
+//
+//
+//
+//
+//
+//
 
 // Open returns a reference to the named file.
 // If name is a hash name then the underlying file is used.
@@ -65,6 +173,7 @@ func (fsys *FS) Open(name string) (fs.File, error) {
 	return f, err
 }
 
+// open
 func (fsys *FS) open(name string) (_ fs.File, hash string, err error) {
 	// Parse filename to see if it contains a hash.
 	// If so, check if hash name matches.
@@ -112,6 +221,8 @@ func (fsys *FS) HashName(name string) string {
 // extension. If no extension exists on filename then the hash is appended.
 // Returns blank string the original filename if hash is blank. Returns a blank
 // string if the filename is blank.
+//
+// TODO: don't export this.
 func FormatName(filename, hash string, hl hashLocation) string {
 	if filename == "" {
 		return ""
@@ -156,6 +267,8 @@ func (fsys *FS) ParseName(filename string) (base, hash string) {
 }
 
 // ParseName splits formatted hash filename into its base & hash components.
+//
+// TODO: don't export this.
 func ParseName(filename string) (base, hash string) {
 	if filename == "" {
 		return "", ""
@@ -199,6 +312,7 @@ type fsHandler struct {
 	fsys *FS
 }
 
+// ServeHTTP is used to serve a hashed static file.
 func (h *fsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Clean up filename based on URL path.
 	filename := r.URL.Path
@@ -252,7 +366,11 @@ func (h *fsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// HashLocationStart sets the hash to be prepended to the start of the filename.
+// HashLocationStart sets the hash to be prepended to the beginning of the filename.
+// This is nice to keep the filename all together, but is a bit ugly for debugging
+// in browser devtools since the on small/narrow screens the hash can take up most
+// of the room.
+//
 // script.min.js becomes a1b2c3...d4e5f6.script.min.js.
 func HashLocationStart() optionFunc {
 	return func(f *FS) {
@@ -260,8 +378,10 @@ func HashLocationStart() optionFunc {
 	}
 }
 
-// HashLocationEnd sets the hash to be appended to the end of the filename with
-// the extension copied after the hash.
+// HashLocationEnd sets the hash to be appended to the end of the filename with the
+// extension copied after the hash. This is nice to keep the filename all together;
+// there really is no downside to this location.
+//
 // script.min.js becomes script.min.a1b2c3...d4e5f6.js.
 func HashLocationEnd() optionFunc {
 	return func(f *FS) {
@@ -271,7 +391,9 @@ func HashLocationEnd() optionFunc {
 
 // HashLocationFirstPeriod sets the hash to be added in the middle of the filename,
 // specifically at the first period in the filename. This was the original designed
-// hash location.
+// hash location. There is really no benefit to this location, and it is a bit ugly
+// since it breaks up the filename.
+//
 // script.min.js -> script-a1b2c3...d4e5f6.min.js
 func HashLocationFirstPeriod() optionFunc {
 	return func(f *FS) {
